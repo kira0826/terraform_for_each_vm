0b9a553959b5862f622239f96d1eabacffbc0cfc
zai <curaca492@gmail.com>
2025-04-07 12:31:02 -0500

    adding readme

--- creation.sh
+++ creation.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Use the directory where is save it as references
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Verify correct folder
+cd "$SCRIPT_DIR" || exit 1
+
 terraform init
 terraform plan 
 terraform apply -auto-approve
